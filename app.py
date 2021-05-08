a603a370808aa189127557233fa59d704de29687
PnzJust <pinzariu.iustin@protonmail.com>
2021-05-08 22:49:39 +0300

    Add tests

--- app.py
+++ app.py
@@ -64,18 +64,18 @@ def read_all(connection, command, timeout=1):
     while resp.endswith(b'--More-- '):
         connection.write(b' ')
         resp += connection.read_until(b'FINAL_INEXISTENT', timeout=timeout)
-    return resp
+    return resp.decode('ascii')
 
 
 def all_interfaces(conn):
-    response = read_all(connection=conn, command='show interfaces status\n').decode('ascii')
+    response = read_all(connection=conn, command='show interfaces status\n')
     re_interfaces = re.findall(r"((Fa|Gi)([0-9]*/)*[0-9]*) +(notconnect|connected|disabled)", response)
     interfaces = [(interface[0], interface[3]) for interface in re_interfaces]
     return interfaces
 
 
 def test_native_vlan(connect):
-    response = read_all(connection=connect, command='show vlan\n').decode('ascii')
+    response = read_all(connection=connect, command='show vlan\n')
     vlan_1 = re.search(r'([1]) +([a-zA-Z-/]+) +', response).groups() 
     if vlan_1[1] == "default":
         warnings.warn(colored("The native vlan should not be vlan 1. \
@@ -90,7 +90,7 @@ def test_switchport_port_security(connect):
     no_port_security_interfaces = ""
     for interface in interfaces:
         if interface[1] == "connected":
-            response = read_all(connection=connect, command='show port-security interface ' + interface[0] + ' \n').decode('ascii')
+            response = read_all(connection=connect, command='show port-security interface ' + interface[0] + ' \n')
             status = re.search(r"Port Security +: (.*)", response).groups()
             if "Enabled\r" not in status[0]:
                 no_port_security_interfaces += interface[0] + " " 
@@ -107,7 +107,7 @@ def test_switchport_port_security_violation(connect):
     no_port_security_violation_interfaces = ""
     for interface in interfaces:
         if interface[1] == "connected":
-            response = read_all(connection=connect, command='show port-security interface ' + interface[0] + ' \n').decode('ascii')
+            response = read_all(connection=connect, command='show port-security interface ' + interface[0] + ' \n')
             status = re.search(r"Violation Mode +: (.*)", response).groups()
             if "Restrict\r" not in status[0] and "Shutdown\r" not in status[0]:
                 no_port_security_violation_interfaces += interface[0] + " " 
@@ -122,23 +122,80 @@ port-security".format(no_port_security_violation_interfaces), "red"))
 def test_cdp(connect):
     # sa fie disabled (cmd: no cdp run) pt ca mesajele cdp sunt 
     # neencriptate/neautentificate
-    pass
+    interfaces = all_interfaces(conn=connect)
+    cdp_interfaces = ""
+    for interface in interfaces:
+        if interface[1] == "connected":
+            response = read_all(connection=connect, command='show cdp interface ' + interface[0] + ' \n')
+            status = re.search(r"(.*) (is) (.*),(.*)", response)
+            if status is  None:
+                continue
+            status = status.groups()
+            if status[2] == "up":
+                cdp_interfaces += interface[0] + " " 
+    if cdp_interfaces:
+        raise Exception(colored("CDP is enabled for interfaces: {}.\
+This missconfiguration could lead to information disclosure because \
+messages are sent unencrypted and unauthenticated. You should disable the cdp on \
+all ports. Eg. command: Switch(config)#no cdp run".format(cdp_interfaces), "red"))
+
 
 def test_acl(connect):
     # acl-ul sa nu aiba vreun 'deny all' in ale sale reguli
-    pass
+    response = read_all(connection=connect, command='show access-lists\n')
+    if "deny   any" in response:
+        warnings.warn(colored("There is a 'deny all' statement in your access \
+list. This statement is by default at the end of all access lists. You \
+could delete this statement and review your access lists.", "yellow"), Warning)
 
-def test_ntp(connect):
-    # de preferat sa fie disabled  (ntp peer)
-    pass
 
 def test_console_password(connect):
     # de preferat sa existe parola pe consola
-    pass
+    response = read_all(connection=connect, command='show running-config | begin line con 0\n', timeout=5).split('\n')
+    line_console_0_response = [response[2][:-1]]
+    for e in response[3:]:
+        if e[0] != ' ':
+            break
+        line_console_0_response.append(e[1:-1])
+    login = False
+    password = False
+    password_encrypted = False
+    for e in line_console_0_response:
+        if e.startswith('login'):
+            login = True
+        elif e.startswith('password'):
+            password = True
+            if len(e.split(" ")) > 2:
+                password_encrypted = True
+
+    if not login:
+        warnings.warn(colored("You forgot to enable your login on your \
+console connection. Without this everybody will be able to connect \
+without a password. Enable command: Switch(config-line)#login", "yellow"), Warning)
+    if not password:
+        warnings.warn(colored("You forgot to set a pass on your \
+console connection. Without this password everybody will be able to connect \
+to your switch.", "yellow"), Warning)
+    elif not password_encrypted:
+        warnings.warn(colored("You forgot to encrypt your password for \
+console connection. Without this the password is stored unencrypted in your \
+config file.", "yellow"), Warning)
+
 
 def test_enable_password(connect):
     # de preferat sa existe parola pe enable
-    pass
+    response = read_all(connection=connect, command='show running-config | include enable password\n', timeout=5)
+    password = response.split('\n')
+    if len(password) < 4:
+        warnings.warn(colored("You forgot to use a password for switch configuration. \
+Without this everybody can config the switch without a password.", "yellow"), Warning)
+    else:
+        password = password[2].split(" ")
+        if len(password) < 4:
+            warnings.warn(colored("You forgot to encrypt your password for \
+switch configuration. Without this the password is stored unencrypted in your \
+config file.", "yellow"), Warning)
+
 
 def test_vtp_password(connect):
     # de preferat sa aiba o parola (sh vtp pass)
